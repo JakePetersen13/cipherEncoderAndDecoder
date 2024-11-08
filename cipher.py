fde1264472ff76135fac426c004ec1b79a48e7f9
Jake Petersen <jdpetersen@cpp.edu>
2024-11-07 18:05:54 -0800

    added basic format of code

--- cipher.py
+++ cipher.py
@@ -1,1 +1,18 @@
-# initial commit
+def main():
+    print("DEBUG: MAIN METHOD")
+    chooseCipher(input("> Entered Desired Cipher: \nCeaser Cipher ('c') \nMono-Alphabetic Cipher ('m') \nPoly-Alphabetic Cipher ('p') \n"))
+    
+
+def chooseCipher(cipherChosen):
+    match cipherChosen.lower():
+        case 'c':
+            print("Ceasar Cipher")
+        case 'm':
+            print("monoalphabetic cipher")
+        case 'p':
+            print("poly alphabetic cipher")
+        case _:
+            print("unkown")
+
+if __name__ == "__main__":
+	main()
